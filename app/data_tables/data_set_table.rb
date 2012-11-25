2261d7d2dce8bb66cd90c79bd54ca1f3fb31a9cb
Michael Smith <smith11235@gmail.com>
2012-11-25 23:08:30 +0000

    ajax initialization of all record displays

--- app/data_tables/data_set_table.rb
+++ app/data_tables/data_set_table.rb
@@ -6,12 +6,20 @@ class DataSetTable
 		@data_set = data_set
 		@display_records = @data_set.records
 
-		if params[:sSearch].present?
+		search # looping through cells instead of with a WHERE
+
+		sort_records
+
+	end
+
+	def search
+		if params[:sSearch].present? && params[:sSearch].size > 0
 			temp_records = []
 			@display_records.map do |record|
 				wanted = false
 				@data_set.fields.map do |field| 
-					wanted = true if record.get_cell( field ).string =~ /#{params[:sSearch]}/i
+					value = record.get_cell( field ).string.to_s
+					wanted = true if value.size > 1 && value =~ /#{params[:sSearch]}/i
 				end
 				temp_records << record if wanted
 			end
@@ -23,7 +31,7 @@ class DataSetTable
 		{
 			sEcho: params[:sEcho].to_i,
 			iTotalRecords: @data_set.records.size,
-			iTotalDisplayRecords: @data_set.records.size,
+			iTotalDisplayRecords: @display_records.size,
 			aaData: get_data
 		}
 	end
@@ -31,10 +39,44 @@ class DataSetTable
 	private
 
 	def get_data
+		# format data for javascript
 		@display_records.map do |record|
 			@data_set.fields.map do |field| 
 				h(record.get_cell( field ).string)
 			end
+		end
+	end
+
+	def sort_records
+		if params[:iSortCol_0].present?
+			sort_field = @data_set.fields[ params[:iSortCol_0].to_i ]
+
+			cell_values = Hash.new
+			# gather the values to be sorted, and their corresponding record indecies 
+			@display_records.each_with_index do |record,i|
+				cell_value = record.get_cell( sort_field ).string
+				cell_values[ cell_value ] ||= Array.new
+				cell_values[ cell_value ] << i
+			end
+
+			temp_records = [] 
+			sorted_cell_values = (cell_values.keys - [nil]).sort # sort them
+			# write them in order
+			sorted_cell_values.each do |cell_value|
+				cell_values[ cell_value ].each do |index|
+					temp_records << @display_records[ index ]
+				end
+			end
+			# add the nill values to the end
+			if cell_values.has_key? nil
+				cell_values[ nil ].each do |index|
+					temp_records << @display_records[ index ]
+				end
+			end
+
+			@display_records = temp_records
+			# check if reversing is needed
+			@display_records.reverse! if params[:sSortDir_0] == "desc" 
 		end
 	end
 
