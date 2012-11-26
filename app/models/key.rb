408d4503277a2822f2d00c1aaea650b9ad1937de
Michael Smith <smith11235@gmail.com>
2012-11-26 03:08:48 +0000

    mostly tinkering here, edited my notes

--- app/models/key.rb
+++ app/models/key.rb
@@ -5,16 +5,17 @@ class Key < ActiveRecord::Base
 
 	def get_records
 		records = Hash.new
+		non_records = Hash.new
 		# for each record in the data set
 		self.data_set.records.each do |record|
 			has_all_fields = true
 			pk_value = ""
 			# make sure it has all the required fields
 			self.key_fields.each do |key_field|
-				next unless has_all_fields
 				cell = record.get_cell( key_field.field )	
 				if cell.field_id.nil? # make sure it's a valid cell
 					has_all_fields = false 
+					pk_value << "{missing:#{key_field.field.name}}"
 				else
 					# build pk value
 					pk_value << "[#{key_field.field.name}:#{cell.string}]"
@@ -23,9 +24,12 @@ class Key < ActiveRecord::Base
 			if has_all_fields
 				records[ pk_value ] ||= Array.new
 				records[ pk_value ] << record 
+			else
+				non_records[ pk_value ] ||= Array.new
+				non_records[ pk_value ] << record 
 			end
 		end
-		return records
+		return records, non_records
 	end
 
 end
