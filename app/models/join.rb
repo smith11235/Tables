065f88aae48f4498d8423ae9205ebf39d2a158bb
Michael Smith <smith11235@gmail.com>
2012-12-08 16:01:02 +0000

    create new join works, now need to make configuration page

--- app/models/join.rb
+++ app/models/join.rb
@@ -1,5 +1,6 @@
 class Join < ActiveRecord::Base
   attr_accessible :name
+
 	belongs_to :left_key, :class_name => "Key"
 	belongs_to :right_key, :class_name => "Key"
 end
--- app/models/key.rb
+++ app/models/key.rb
@@ -7,6 +7,9 @@ class Key < ActiveRecord::Base
 	has_many :key_records, :dependent => :destroy
 	has_many :records, :through => :key_records
 
+	has_many :left_joins, :class_name => "Join", :foreign_key => "left_key_id"
+	has_many :right_joins, :class_name => "Join", :foreign_key => "right_key_id"
+
 	def unique_field_names
 		return self.fields.collect {|field| field.name }
 	end
